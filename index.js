edbb19bc5d0d9a213f2f4719767023687feb7be7
Brenda Nahomi Quispe Gonzales <adnerbi.1107@gmail.com>
2022-05-12 22:24:30 -0500

    Add routes to index file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { connection } = require("./config/db");
 const users = require("./routes/users");
+const enterprise = require("./routes/enterprise");
 const auth = require("./routes/auth");
+const job = require("./routes/job");
 const app = express();
 app.use(express.json());
 connection();
@@ -9,7 +11,8 @@ connection();
 // routes
 users(app);
 auth(app);
-
+enterprise(app);
+job(app);
 app.listen(3008, () => {
   console.log(`Server running on port 3008`);
 });
