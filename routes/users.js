fc25a728f784b463e743e38e09d3ec65d726a8d9
Brenda Nahomi Quispe Gonzales <adnerbi.1107@gmail.com>
2022-05-12 22:22:52 -0500

    Add middlewares to user routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const {
+  authValidation,
+  userValidation,
+} = require("../middlewares/authValidation");
 const UserService = require("../services/users");
 
 function users(app) {
@@ -6,21 +10,36 @@ function users(app) {
   const userService = new UserService();
 
   app.use("/api/users", router);
-  router.get("/", async (_, res) => {
+  router.get("/", authValidation, userValidation("admin"), async (_, res) => {
     const users = await userService.getAll();
     return res.json(users);
   });
-  router.post("/", async (req, res) => {
-    const users = await userService.create(req.body);
-    return res.json(users);
-  });
-  router.put("/:id", async (req, res) => {
-    const user = await userService.update(req.params.id, req.body);
-    return res.json(user);
-  });
-  router.delete("/:id", async (req, res) => {
-    const user = await userService.delete(req.params.id);
-    return res.json(user);
-  });
+  router.post(
+    "/",
+    authValidation,
+    userValidation("admin"),
+    async (req, res) => {
+      const users = await userService.create(req.body);
+      return res.json(users);
+    }
+  );
+  router.put(
+    "/:id",
+    authValidation,
+    userValidation("admin"),
+    async (req, res) => {
+      const user = await userService.update(req.params.id, req.body);
+      return res.json(user);
+    }
+  );
+  router.delete(
+    "/:id",
+    authValidation,
+    userValidation("admin"),
+    async (req, res) => {
+      const user = await userService.delete(req.params.id);
+      return res.json(user);
+    }
+  );
 }
 module.exports = users;
